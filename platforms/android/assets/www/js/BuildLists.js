4fee6962c061631f4f25c275056f6e24751eee9a
sjpayne <sjpayne@sfu.ca>
2016-03-12 12:53:42 -0800

    Trying to fix

--- platforms/android/assets/www/js/BuildLists.js
+++ platforms/android/assets/www/js/BuildLists.js
@@ -1,6 +1,7 @@
 //Modified from http://www.w3schools.com/json/json_example.asp
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 $('#Nearest').live('pagecreate', function() {
 	// Set this to the location on the AWS server where the JSON file is stored 
@@ -16,26 +17,47 @@ $('#Nearest').live('pagecreate', function() {
 		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
 			myFunction(xmlhttp.responseText);
 		}
-	}
-	xmlhttp.open("GET", url, true);
-	xmlhttp.send();
+=======
+/* Set this to the location on the AWS server where the JSON file is stored */
+var xmlhttp = new XMLHttpRequest();
+var url = "data/testdata.json";
 
-	/* Convert JSON to HTML */
-	function myFunction(response) {
-		var arr = JSON.parse(response);
-		var out = "<table>"; 
-		
-		out += "<tr><td> Event </td><td> Location </td><td> Time </td></tr>";
-		
-		for(var i = 0; i < arr.length; i++) {
-			out += "<tr><td>" + arr[i].Name +
-			"</td><td>" + arr[i].Location +
-			"</td><td>" + arr[i].Time +
-			"</td></tr>";
-		}
-		out += "</table>";
-
-		/* To Do: Fix bug where table doesn't display on the initial load */
-		document.getElementById("EventList").innerHTML = out;
+/* Request json */
+xmlhttp.onreadystatechange=function() {
+	if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+		myFunction(xmlhttp.responseText);
+>>>>>>> parent of 5956bc1... Fix GET and List
 	}
-});
+}
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+
+/* To Do: categorize data*/
+// SORT DATA 
+// BY Time
+
+// By Type
+
+// By Location (alphabetical)
+
+// By Distance
+
+
+/* Convert JSON to HTML */
+function myFunction(response) {
+	var arr = JSON.parse(response);
+	var out = "<table>"; 
+	
+	out += "<tr><td> Event </td><td> Location </td><td> Time </td></tr>";
+	
+	for(var i = 0; i < arr.length; i++) {
+		out += "<tr><td>" + arr[i].Name +
+		"</td><td>" + arr[i].Location +
+		"</td><td>" + arr[i].Time +
+		"</td></tr>";
+	}
+	out += "</table>";
+
+	/* To Do: Fix bug where table doesn't display on the initial load */
+	document.getElementById("EventList").innerHTML = out;
+}
--- www/js/BuildLists.js
+++ www/js/BuildLists.js
@@ -1,37 +1,44 @@
 //Modified from http://www.w3schools.com/json/json_example.asp
 
-$('#Nearest').live('pagecreate', function() {
-	/* Set this to the location on the AWS server where the JSON file is stored */
-	var xmlhttp = new XMLHttpRequest();
-	var url = "http://ec2-54-218-45-124.us-west-2.compute.amazonaws.com/Vicinity.json";
-	//var url = "data/AllEvents.json";
+/* Set this to the location on the AWS server where the JSON file is stored */
+var xmlhttp = new XMLHttpRequest();
+var url = "data/testdata.json";
+
+/* Request json */
+xmlhttp.onreadystatechange=function() {
+	if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+		myFunction(xmlhttp.responseText);
+	}
+}
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+
+/* To Do: categorize data*/
+// SORT DATA 
+// BY Time
+
+// By Type
+
+// By Location (alphabetical)
+
+// By Distance
+
+
+/* Convert JSON to HTML */
+function myFunction(response) {
+	var arr = JSON.parse(response);
+	var out = "<table>"; 
 	
-	/* Request json */
-	xmlhttp.onreadystatechange=function() {
-		alert(xmlhttp.status);
-		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-			myFunction(xmlhttp.responseText);
-		}
+	out += "<tr><td> Event </td><td> Location </td><td> Time </td></tr>";
+	
+	for(var i = 0; i < arr.length; i++) {
+		out += "<tr><td>" + arr[i].Name +
+		"</td><td>" + arr[i].Location +
+		"</td><td>" + arr[i].Time +
+		"</td></tr>";
 	}
-	xmlhttp.open("GET", url, true);
-	xmlhttp.send();
+	out += "</table>";
 
-	/* Convert JSON to HTML */
-	function myFunction(response) {
-		var arr = JSON.parse(response);
-		var out = "<table>"; 
-		
-		out += "<tr><td> Event </td><td> Location </td><td> Time </td></tr>";
-		
-		for(var i = 0; i < arr.length; i++) {
-			out += "<tr><td>" + arr[i].Name +
-			"</td><td>" + arr[i].Location +
-			"</td><td>" + arr[i].Time +
-			"</td></tr>";
-		}
-		out += "</table>";
-
-		/* To Do: Fix bug where table doesn't display on the initial load */
-		document.getElementById("EventList").innerHTML = out;
-	}
-});
+	/* To Do: Fix bug where table doesn't display on the initial load */
+	document.getElementById("EventList").innerHTML = out;
+}
