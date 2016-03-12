7454c04076fc18748403d2259763aab8060e6561
sjpayne <sjpayne@sfu.ca>
2016-03-12 14:04:52 -0800

    Update JQuery requests from conflicting version

--- www/js/BuildLists.js
+++ www/js/BuildLists.js
@@ -1,44 +1,39 @@
 //Modified from http://www.w3schools.com/json/json_example.asp
 
-/* Set this to the location on the AWS server where the JSON file is stored */
-var xmlhttp = new XMLHttpRequest();
-var url = "data/testdata.json";
 
-/* Request json */
-xmlhttp.onreadystatechange=function() {
-	if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-		myFunction(xmlhttp.responseText);
-	}
-}
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
-
-/* To Do: categorize data*/
-// SORT DATA 
-// BY Time
-
-// By Type
-
-// By Location (alphabetical)
-
-// By Distance
-
-
-/* Convert JSON to HTML */
-function myFunction(response) {
-	var arr = JSON.parse(response);
-	var out = "<table>"; 
+$(document).on('pagecreate', '#Nearest', function() {
+	// Set this to the location on the AWS server where the JSON file is stored 
+	//alert("AUGH WHY DID THEY REMOVE LIVE?!?!?");
+	var xmlhttp = new XMLHttpRequest();
+	var url = "data/testdata.json";
+	//var url = "data/AllEvents.json";
 	
-	out += "<tr><td> Event </td><td> Location </td><td> Time </td></tr>";
-	
-	for(var i = 0; i < arr.length; i++) {
-		out += "<tr><td>" + arr[i].Name +
-		"</td><td>" + arr[i].Location +
-		"</td><td>" + arr[i].Time +
-		"</td></tr>";
+	// Request json 
+	xmlhttp.onreadystatechange=function() {
+		//alert(xmlhttp.status);
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			myFunction(xmlhttp.responseText);
+		}
 	}
-	out += "</table>";
+	xmlhttp.open("GET", url, true);
+	xmlhttp.send();
 
-	/* To Do: Fix bug where table doesn't display on the initial load */
-	document.getElementById("EventList").innerHTML = out;
-}
+	// Convert JSON to HTML 
+	function myFunction(response) {
+		var arr = JSON.parse(response);
+		var out = "<table>"; 
+		
+		out += "<tr><td> Event </td><td> Location </td><td> Time </td></tr>";
+		
+		for(var i = 0; i < arr.length; i++) {
+			out += "<tr><td>" + arr[i].Name +
+			"</td><td>" + arr[i].Location +
+			"</td><td>" + arr[i].Time +
+			"</td></tr>";
+		}
+		out += "</table>";
+
+		// To Do: Fix bug where table doesn't display on the initial load 
+		document.getElementById("EventList").innerHTML = out;
+	}
+});
