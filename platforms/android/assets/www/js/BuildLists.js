377a3c7c4f82cf5f5f3a851f22a102e68003779b
sjpayne <sjpayne@sfu.ca>
2016-03-12 12:52:06 -0800

    Trying to fix cupported file

--- platforms/android/assets/www/js/BuildLists.js
+++ platforms/android/assets/www/js/BuildLists.js
@@ -1,15 +1,18 @@
 //Modified from http://www.w3schools.com/json/json_example.asp
 
+<<<<<<< HEAD
 
 $('#Nearest').live('pagecreate', function() {
 	// Set this to the location on the AWS server where the JSON file is stored 
+=======
+$('#Nearest').live('pagecreate', function() {
+	/* Set this to the location on the AWS server where the JSON file is stored */
+>>>>>>> parent of 6cb5e85... Try to fix json get
 	var xmlhttp = new XMLHttpRequest();
-	var url = "https://mjdgbkaht5.execute-api.us-west-2.amazonaws.com/Beta";
-	//var url = "data/AllEvents.json";
-	
-	// Request json 
+	var url = "data/AllEvents.json";
+
+	/* Request json */
 	xmlhttp.onreadystatechange=function() {
-		//alert(xmlhttp.status);
 		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
 			myFunction(xmlhttp.responseText);
 		}
@@ -17,7 +20,7 @@ $('#Nearest').live('pagecreate', function() {
 	xmlhttp.open("GET", url, true);
 	xmlhttp.send();
 
-	// Convert JSON to HTML 
+	/* Convert JSON to HTML */
 	function myFunction(response) {
 		var arr = JSON.parse(response);
 		var out = "<table>"; 
@@ -32,7 +35,7 @@ $('#Nearest').live('pagecreate', function() {
 		}
 		out += "</table>";
 
-		// To Do: Fix bug where table doesn't display on the initial load 
+		/* To Do: Fix bug where table doesn't display on the initial load */
 		document.getElementById("EventList").innerHTML = out;
 	}
 });
