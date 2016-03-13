b6e79021668b5f373b772158dd2191ae0f779622
sjpayne <sjpayne@sfu.ca>
2016-03-13 16:58:08 -0700

    Fix for Map crash on updated JQuery and when using custom icon

--- www/js/Map.js
+++ www/js/Map.js
@@ -6,48 +6,15 @@ var icons = {
   iconImage:'www/img/MapIcon.png'
 };
 
-var url = "data/testdata.json";
+var url = "data/testdata2.json";
 //var url = "https://mjdgbkaht5.execute-api.us-west-2.amazonaws.com/SideTracked";
 
-var ActivityTitle = "Test";
-var ActivityTime = "Aughh";
-
-
-
-/*$(document).on('pagecreate', '#Test', function() {
-	var url = "data/testdata.json";
-	//var url = "https://mjdgbkaht5.execute-api.us-west-2.amazonaws.com/SideTracked";
-
-	//Have server only return nearby locations
-	$.getJSON(url, {get_param: 'value' }, function( data ){ 
-		var Nearby = "<table>"; 
-		Nearby += "<tr><td> <h2>Event</h2> </td><td> <h2>Location</h2> </td><td> <h2>Time</h2> </td></tr>";
-		
-		$.each(data, function(index, element) {
-			Nearby += "<tr><td>" + element.Name +
-			"</td><td>" + element.Location +
-			"</td><td>" + element.Time +
-			"</td></tr>";
-		});
-		
-		Nearby += "</table>";
-		document.getElementById("Test").innerHTML = Nearby;
-	});
-});*/
-
+//var ActivityTitle = "";
+//var ActivityTime = "";
 
 
 /* Main function */
 document.addEventListener("deviceready", function(){
-	
-		/*$.getJSON(url, function(data){
-			
-			alert("I did something!");
-		});*/
-	
-	
-	
-	
 		var div = document.getElementById("MapCanvas");
 		
 		/* Using set location of Vancouver for now, since data on events is 
@@ -58,11 +25,20 @@ document.addEventListener("deviceready", function(){
 		map = plugin.google.maps.Map.getMap(div);
 		
 		
+		
+		
 		/* Re-enable map after navigating away */
 		map.setVisible(true);
 		
 		/* Enable button to make map full screen */
 		map.addEventListener(plugin.google.maps.event.MAP_READY, MapReady);
+		
+		/*Set so that it is possible to navigate away from the map page*/
+		map.addEventListener(plugin.google.maps.event.MAP_READY, NavigateAway);
+		
+		
+		
+		
 		
 		/* Set starting location and zoom level for map */ 
 		map.addEventListener(plugin.google.maps.event.MAP_READY, function(){
@@ -73,51 +49,90 @@ document.addEventListener("deviceready", function(){
 			// Get location of user => Need to build onSuccess and onError 
 			//map.getMyLocation(onSuccess, onError);
 			
-			// Create map markers at appropriate locations
-			map.addMarker({
-				position: City,
-				icon: {
-					'url': icons.iconImage,
-					'size': {
-						'width': 25,
-						'height': 25
-					}
-				},
-				'title': "First Pin",
-				'snippet': "",
-				"styles": {
-					"maxWidth": "60%" 
-				}
-			}, 	function(marker) {
-					marker.showInfoWindow();
-					marker.setTitle(ActivityTitle);
-					marker.setSnippet(ActivityTime);
+			
+			$.getJSON(url, function(data){
+				$.each(data, function(index, element) {
+					var ActivityTitle = element.Name;
+					var ActivityTime = element.Time;
+					var NewLocation = new plugin.google.maps.LatLng(element.LatLong[0], element.LatLong[1]);
 					
-					map.addEventListener("ZoomOut", function(){
-						marker.setVisible(false);
+					// Create map markers at appropriate locations
+					map.addMarker({
+						position: ActivityLocation,
+						icon: {
+							'url': icons.iconImage,
+							'size': {
+								'width': 18,
+								'height': 18
+							}
+						},
+						'title': "",
+						'snippet': "",
+						"styles": {
+							"maxWidth": "60%" 
+						}
+					}, 	function(marker) {
+							marker.setPosition(NewLocation);
+							marker.setTitle(ActivityTitle);
+							marker.setSnippet(ActivityTime);
+							
+							map.addEventListener("ZoomOut", function(){
+								marker.setVisible(false);
+							});
+							map.addEventListener("ZoomIn", function(){
+								marker.setVisible(true);
+								marker.setIcon({
+									'url': icons.iconImage,
+									'size': {
+										'width' : 18,
+										'height': 18
+									}
+								});
+							});
+							map.addEventListener("ZoomMax", function(){
+								marker.setVisible(true);
+								marker.setIcon({
+									'url': icons.iconImage,
+									'size': {
+										'width' : 12,
+										'height': 12
+									}
+								});
+							});
+						
 					});
-					map.addEventListener("ZoomIn", function(){
-						marker.setVisible(true);
-					});
+				});
 				
+				//alert("It worked!");
 			});
 			
+							
 		});
 		
 		map.on(plugin.google.maps.event.CAMERA_CHANGE, onMapCameraChanged);
 		
-}, false);
+		
 
+		
+}, false);
 
 /* Function for making map full screen */ 
 function MapReady(){
-    var button = document.getElementById("MapFullScreen");
-    button.addEventListener("click", onBtnClicked, false);
+	var button = document.getElementById("MapFullScreen");
+	button.addEventListener("click", onBtnClicked, false);
 }
 
 /* Enable map to be minimized and maximixed on click */ 
 function onBtnClicked(){
-    map.showDialog();
+	map.showDialog();
+}
+
+
+/*Set so that it is possible to navigate away*/
+function NavigateAway(){
+	var button = document.getElementById("NavigateAway");
+	button.addEventListener("tap", CloseMap, false);
+	
 }
 
 /* Prevent map from displaying on additional pages */ 
@@ -128,10 +143,13 @@ function CloseMap(){
 
 function onMapCameraChanged(position) {
 	map.getCameraPosition(function(camera) {
-		if (camera.zoom < 14 ){
+		if (camera.zoom < 12 ){
 			map.trigger("ZoomOut");
 		}
-		if (camera.zoom > 14 ){
+		if (camera.zoom <= 13  && camera.zoom >= 12){
+			map.trigger("ZoomMax");
+		}
+		if (camera.zoom > 13 ){
 			map.trigger("ZoomIn");
 		}
 	});
